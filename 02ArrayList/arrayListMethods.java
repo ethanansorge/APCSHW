f4b62a4140829b37f53e167ad3e2b54fdd5dfa54
ethanansorge <ethanansorge@gmail.com>
2014-11-14 12:51:27 -0500

    Update arrayListMethods.java

--- 02ArrayList/arrayListMethods.java
+++ 02ArrayList/arrayListMethods.java
@@ -1,27 +1,47 @@
 import java.util.Random;
 import java.util.*;
 public class arrayListMethods{
-    public void collapseDuplicats(ArrayList<Integer>L){
-        ArrayList<Integer> newList = new ArrayList<Integer>();
-	int i = 0;
-	while ( i < L.size() - 1){
-	    if ( L.get(i) != L.get(i + 1)){
-		newList.set(i, L.get(i));
-		i = i + 1;
-	    }
-	}
-	L = newList;
-    }
-    public static void randomize( ArrayList<Integer> L) { 
-	int i = 0;
-	Random r = new Random();
-	while (i < L.size()){
-	    L.set(r.nextInt(L.size()), L.get(i));
-	    i = i + 1;
-	}
-    }
-    public static void main(String [] args){
-	ArrayList<Integer> L = new ArrayList<Integer>();
-    }
-
+public static void collapseDuplicats(ArrayList<Integer>L){
+int i = 0;
+while ( i < L.size() - 1){
+if ( L.get(i).equals(L.get(i + 1))){
+L.remove(i);
+}else{
+i = i + 1;
+}
+}
+}
+public static void randomize( ArrayList<Integer> L) {
+int i = 0;
+int x = 0;
+Random r = new Random();
+while (i < L.size()){
+x = r.nextInt(L.size());
+L.set(x, L.get(i));
+L.set(i, x);
+i = i + 1;
+}
+}
+public static void main(String [] args){
+ArrayList<Integer> L = new ArrayList<Integer>();
+int c = 0;
+while (c < 10){
+L.add(c);
+c = c + 1;
+}
+randomize(L);
+System.out.println(L);
+L.set(0, 1);
+L.set(1, 1);
+L.set(2, 2);
+L.set(3, 2);
+L.set(4, 2);
+L.set(5, 3);
+L.set(6, 4);
+L.set(7, 5);
+L.set(8, 6);
+L.set(9, 6);
+collapseDuplicats(L);
+System.out.println(L);
+}
 }
