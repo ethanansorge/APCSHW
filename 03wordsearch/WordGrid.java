4dfabb0a72d3564ff4410f901a14da567bca7c05
ethanansorge <ethanansorge@gmail.com>
2014-12-01 22:51:49 -0500

    Put main into driver.

--- 03wordsearch/WordGrid.java
+++ 03wordsearch/WordGrid.java
@@ -119,22 +119,4 @@ public class WordGrid{
 	}
 	return true;
     }
-    public static void main (String [] args) throws FileNotFoundException {
-	ArrayList<String> words = new ArrayList<String>();
-	File text = new File ("words.txt");
-	Scanner scnr = new Scanner(text);
-	int lineNumber = 1;
-	while(scnr.hasNextLine()){
-	    String line = scnr.nextLine();
-	    words.add(line);
-	}
-	WordGrid a = new WordGrid();
-	int i = 0;
-	while ( i < words.size()){
-	    Random rand = new Random();
-	    a.addWord(words.get(i),rand.nextInt(10), rand.nextInt(10), rand.nextInt(2) + 1,rand.nextInt(2) + 1);
-	    i = i + 1;
-	}
-	System.out.println(a);
-    }
 }
