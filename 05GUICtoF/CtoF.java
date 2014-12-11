da1790db3e1ff0432042cc42b6cc135e4afdcf36
ethanansorge <ethanansorge@gmail.com>
2014-12-10 20:10:49 -0500

    CtoF works

--- 05GUICtoF/CtoF.java
+++ 05GUICtoF/CtoF.java
@@ -1,11 +1,11 @@
+import java.awt.event.*;
 import javax.swing.*;
 import java.awt.*;
-public class CtoF extends JFrame {
+public class CtoF extends JFrame implements ActionListener{
     private Container pane;
-
     private JButton b,c;
     private JLabel l;
-    private JTextField t;
+    private JTextField text;
     public CtoF() {
 	this.setTitle("Celsius to Fahrenheit");
 	this.setSize(600,400);
@@ -18,17 +18,32 @@ public class CtoF extends JFrame {
 
 	b = new JButton("Celsius to Fahrenheit");
 	c = new JButton("Fahrenheit to Celsius");
-	l = new JLabel("Click Here to Convert a Temperature",null,JLabel.CENTER);
-	t = new JTextField(12);
+	l = new JLabel("Enter Text And Click A Button to Convert a Temperature",null,JLabel.CENTER);
+	text = new JTextField(40);
 	
 	pane.add(l);
 	pane.add(b);
-	pane.add(t);
+	pane.add(text);
 	pane.add(c);
+	
+	b.setActionCommand("Celsius to Fahrenheit");
+	b.addActionListener(this);
+	c.setActionCommand("Fahrenheit to Celsius");
+	c.addActionListener(this);
+
     }
-    
+    public void actionPerformed(ActionEvent e){
+	String action = e.getActionCommand();
+	double s = Double.parseDouble(text.getText());
+	if (action.equals("Celsius to Fahrenheit")){
+	    s = ((s * 9.0) / 5.0) + 32.0;
+	}else{
+	    s = ((s - 32.0) * 5.0) / 9.0;
+	}
+	text.setText("" + s);
+    }
     public static void main(String[] args) {
-        CtoF g = new CtoF();
-        g.setVisible(true);
+	CtoF g = new CtoF();
+	g.setVisible(true);
     }
 }
